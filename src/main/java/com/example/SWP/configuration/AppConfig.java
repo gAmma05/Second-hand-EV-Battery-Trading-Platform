9f3f5704dccbf96b7719e0f3eb72e6f3d32b304a
loccol9696 <loccol9696@gmail.com>
2025-10-06 16:24:06 +0700

    sua verifyToken: them withIssuer, bo sung SecurityConfig, AuthenticationFilter

--- src/main/java/com/example/SWP/configuration/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.SWP.configuration;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class AppConfig {
-
-}
