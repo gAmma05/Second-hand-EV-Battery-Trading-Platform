94809eb143e4a006461751ce28e405882552f157
loccol9696 <loccol9696@gmail.com>
2025-10-07 19:14:28 +0700

    them attribute entity user, them api xem profile

--- src/main/java/com/example/SWP/controller/user/UserController.java
+++ src/main/java/com/example/SWP/controller/user/UserController.java
@@ -1,14 +1,13 @@
 package com.example.SWP.controller.user;
 
+import com.example.SWP.dto.request.UpdateUserRequest;
 import com.example.SWP.dto.response.ApiResponse;
 import com.example.SWP.dto.response.UserProfileResponse;
 import com.example.SWP.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -32,4 +31,21 @@ public class UserController {
                 .data(profile)
                 .build());
     }
+
+    @PutMapping("/me")
+    public ResponseEntity<ApiResponse<UserProfileResponse>> updateProfile(
+            Authentication authentication,
+            @RequestBody UpdateUserRequest request
+    ) {
+        UserProfileResponse updatedProfile = userService.updateUserProfile(authentication, request);
+
+        return ResponseEntity.ok(ApiResponse.<UserProfileResponse>builder()
+                .success(true)
+                .message("Profile updated successfully")
+                .data(updatedProfile)
+                .build());
+    }
+
+
+
 }
