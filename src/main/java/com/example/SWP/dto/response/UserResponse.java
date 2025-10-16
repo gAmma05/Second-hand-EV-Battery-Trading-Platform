518f0016698bc02e82f7782149c9ca243ec0ef7a
loccol9696 <loccol9696@gmail.com>
2025-10-16 10:10:55 +0700

    them attribute trong user response

--- src/main/java/com/example/SWP/dto/response/UserResponse.java
+++ src/main/java/com/example/SWP/dto/response/UserResponse.java
@@ -1,19 +1,26 @@
 package com.example.SWP.dto.response;
 
-import com.example.SWP.enums.AuthProvider;
 import com.example.SWP.enums.Role;
 import lombok.Builder;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Data
 @Builder
 public class UserResponse {
     private String email;
     private String fullName;
-    private Role role;
-    private AuthProvider provider;
-    private String avatar;
-    private String phone;
     private String address;
+    private String phone;
+    private String avatar;
+    private String storeName;
+    private String storeDescription;
+    private String socialMedia;
+    private int remainingPosts;
+    private Role role;
     private boolean status;
+    private Long sellerPackageId;
+    private LocalDateTime planExpiry;
 }
+
