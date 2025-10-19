37b5a7e8a8781399ed2e602c19730e385c67a1ec
gAmma05 <minedung2005@gmail.com>
2025-10-19 20:29:45 +0700

    Add order approve/reject feature for seller, with notification

---
+++ src/main/java/com/example/SWP/enums/ContractStatus.java
@@ -0,0 +1,7 @@
+package com.example.SWP.enums;
+
+public enum ContractStatus {
+    PENDING,
+    CANCELLED,
+    SIGNED
+}
--- src/main/java/com/example/SWP/service/seller/SellerService.java
+++ src/main/java/com/example/SWP/service/seller/SellerService.java
@@ -2,15 +2,17 @@ package com.example.SWP.service.seller;
 
 import com.example.SWP.dto.request.buyer.UpgradeToSellerRequest;
 
+import com.example.SWP.dto.response.seller.RejectOrderResponse;
+import com.example.SWP.entity.Order;
 import com.example.SWP.entity.PriorityPackage;
 import com.example.SWP.entity.SellerPackage;
 import com.example.SWP.entity.User;
+import com.example.SWP.enums.OrderStatus;
 import com.example.SWP.enums.Role;
 import com.example.SWP.enums.SellerPackageType;
 import com.example.SWP.exception.BusinessException;
-import com.example.SWP.repository.PriorityPackageRepository;
-import com.example.SWP.repository.SellerPackageRepository;
-import com.example.SWP.repository.UserRepository;
+import com.example.SWP.repository.*;
+import com.example.SWP.service.notification.NotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.security.core.Authentication;
@@ -25,7 +27,9 @@ public class SellerService {
 
     UserRepository userRepository;
     PriorityPackageRepository priorityPackageRepository;
-    SellerPackageRepository  sellerPackageRepository;
+    SellerPackageRepository sellerPackageRepository;
+    OrderRepository orderRepository;
+    NotificationService notificationService;
 
     public void upgradeToSeller(Authentication authentication, UpgradeToSellerRequest request) {
         String email = authentication.getName();
@@ -52,6 +56,61 @@ public class SellerService {
         user.setSocialMedia(request.getSocialMedia());
         user.setRemainingPosts(3);
         userRepository.save(user);
+    }
+
+    public void approveOrder(Authentication authentication, Long orderId) {
+        String email = authentication.getName();
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new BusinessException("User does not exist", 404));
+
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new BusinessException("Order does not exist", 404));
+
+        if (order.getStatus().equals(OrderStatus.APPROVED)) {
+            throw new BusinessException("Order is already approved", 400);
+        } else if (order.getStatus().equals(OrderStatus.REJECTED)) {
+            throw new BusinessException("You can't approve this order because it's already rejected", 400);
+        } else if (order.getStatus().equals(OrderStatus.PENDING)) {
+            order.setStatus(OrderStatus.APPROVED);
+
+        }
+
+        String buyerEmail = order.getBuyer().getEmail();
+        if (buyerEmail == null) {
+            throw new BusinessException("Buyer email is not found", 404);
+        }
+
+        sendNotification(buyerEmail, "Order Approved", "Your order has been approved by the admin");
+        orderRepository.save(order);
+    }
+
+    public void rejectOrder(Authentication authentication, RejectOrderResponse response) {
+        String email = authentication.getName();
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new BusinessException("User does not exist", 404));
+
+        Order order = orderRepository.findById(response.getOrderId())
+                .orElseThrow(() -> new BusinessException("Order does not exist", 404));
+
+        if (order.getStatus().equals(OrderStatus.REJECTED)) {
+            throw new BusinessException("Order is already rejected", 400);
+        } else if (order.getStatus().equals(OrderStatus.APPROVED)) {
+            throw new BusinessException("You can't reject this order because it's already approved", 400);
+        } else if (order.getStatus().equals(OrderStatus.PENDING)) {
+            order.setStatus(OrderStatus.REJECTED);
+        }
+
+        String buyerEmail = order.getBuyer().getEmail();
+        if (buyerEmail == null) {
+            throw new BusinessException("Buyer email is not found", 404);
+        }
+
+        sendNotification(buyerEmail, "Order Rejected", "Your order has been rejected with reason: " + response.getReason());
+        orderRepository.save(order);
+    }
+
+    private void sendNotification(String email, String title, String content) {
+        notificationService.sendNotificationToOneUser(email, title, content);
     }
 
 
