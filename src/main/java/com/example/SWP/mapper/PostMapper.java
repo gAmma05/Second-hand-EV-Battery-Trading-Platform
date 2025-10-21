111ae9c5fadc8493f12842c29d38e6e38c160ec1
loccol9696 <loccol9696@gmail.com>
2025-10-21 20:53:59 +0700

    fix admin get post, fix lay trusted ma trusted bang false

--- src/main/java/com/example/SWP/mapper/PostMapper.java
+++ src/main/java/com/example/SWP/mapper/PostMapper.java
@@ -2,11 +2,29 @@ package com.example.SWP.mapper;
 
 import com.example.SWP.dto.response.seller.PostResponse;
 import com.example.SWP.entity.Post;
+import com.example.SWP.entity.PostImage;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface PostMapper {
+
+    @Mapping(target = "trusted", expression = "java(post.isTrusted())")
+    @Mapping(target = "images", source = "images", qualifiedByName = "mapImages")
+    PostResponse toPostResponse(Post post);
+
     List<PostResponse> toPostResponseList(List<Post> posts);
+
+    @Named("mapImages")
+    default List<String> mapImages(List<PostImage> images) {
+        if (images == null) return Collections.emptyList();
+        return images.stream()
+                .map(PostImage::getImageUrl)
+                .collect(Collectors.toList());
+    }
 }
