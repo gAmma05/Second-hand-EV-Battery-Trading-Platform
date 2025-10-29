8e93662d10abe7547af2fd7128ee1e0cce3c8f75
gAmma05 <minedung2005@gmail.com>
2025-10-29 10:06:04 +0700

    Rename and refactor the reject order request to correct directory

--- src/main/java/com/example/SWP/repository/ComplaintRepository.java
+++ src/main/java/com/example/SWP/repository/ComplaintRepository.java
@@ -1,13 +1,19 @@
 package com.example.SWP.repository;
 
 import com.example.SWP.entity.Complaint;
+import com.example.SWP.enums.ComplaintStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ComplaintRepository extends JpaRepository<Complaint, Long> {
     List<Complaint> findByOrder_Buyer_Id(Long buyerId);
+
     List<Complaint> findByOrder_Seller_Id(Long sellerId);
+
+    List<Complaint> findByStatus(ComplaintStatus status);
+
     Complaint findByIdAndOrder_Buyer_Id(Long complaintId, Long buyerId);
+
     Complaint findByIdAndOrder_Seller_Id(Long complaintId, Long sellerId);
 }
