c2964195e3e52821461bfe57a8f141c1a004b824
gAmma05 <minedung2005@gmail.com>
2025-11-01 10:57:57 +0700

    Change into order id

--- src/main/java/com/example/SWP/service/buyer/BuyerFeedbackService.java
+++ src/main/java/com/example/SWP/service/buyer/BuyerFeedbackService.java
@@ -3,15 +3,12 @@ package com.example.SWP.service.buyer;
 import com.example.SWP.dto.request.buyer.FeedbackRequest;
 import com.example.SWP.entity.Feedback;
 import com.example.SWP.entity.Order;
-import com.example.SWP.entity.Post;
 import com.example.SWP.entity.User;
 import com.example.SWP.enums.OrderStatus;
-import com.example.SWP.enums.PostStatus;
 import com.example.SWP.exception.BusinessException;
 import com.example.SWP.mapper.FeedbackMapper;
 import com.example.SWP.repository.FeedbackRepository;
 import com.example.SWP.repository.OrderRepository;
-import com.example.SWP.repository.PostRepository;
 import com.example.SWP.repository.UserRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -30,8 +27,6 @@ public class BuyerFeedbackService {
 
     UserRepository userRepository;
 
-    PostRepository postRepository;
-
     OrderRepository orderRepository;
 
     FeedbackMapper feedbackMapper;
@@ -43,18 +38,16 @@ public class BuyerFeedbackService {
                 () -> new BusinessException("User not found", 404)
         );
 
-        Post post = postRepository.findById(request.getPostId()).orElseThrow(
-                () -> new BusinessException("Post not found", 404)
+        Order order = orderRepository.findById(request.getOrderId()).orElseThrow(
+                () -> new BusinessException("Order not found", 404)
         );
 
-        if (!Objects.equals(post.getStatus(), PostStatus.SOLD)) {
-            throw new BusinessException("You cannot feedback as this product is not sold yet nor sold by you", 400);
+        if (order.getBuyer().getId() != user.getId()) {
+            throw new BusinessException("Order này không phải của bạn", 400);
         }
 
-        Order order = orderRepository.findByPost_IdAndBuyer_IdAndStatus(post.getId(), user.getId(), OrderStatus.DONE);
-
-        if (order == null) {
-            throw new BusinessException("You cannot feedback as you have not ordered this product yet", 400);
+        if (!Objects.equals(order.getStatus(), OrderStatus.DONE)) {
+            throw new BusinessException("Đơn hàng của bạn chưa được giao hoặc bạn chưa nhận, bạn không thể feedback trên order này", 400);
         }
 
         Feedback feedback = feedbackMapper.toFeedback(request);
