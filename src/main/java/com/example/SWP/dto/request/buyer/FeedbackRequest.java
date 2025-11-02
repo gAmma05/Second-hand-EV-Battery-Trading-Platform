e30616e0dfbcbc42ba1dd8f6954519deec0ef502
gAmma05 <minedung2005@gmail.com>
2025-11-02 21:03:38 +0700

    Finish feedback flow, will test later

--- src/main/java/com/example/SWP/dto/request/buyer/FeedbackRequest.java
+++ src/main/java/com/example/SWP/dto/request/buyer/FeedbackRequest.java
@@ -1,6 +1,7 @@
 package com.example.SWP.dto.request.buyer;
 
 import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,6 +19,8 @@ public class FeedbackRequest {
     Long orderId;
 
     @NotNull(message = "Rating is required")
+    @Min(value = 1, message = "Rating must be between 1 and 5")
+    @Max(value = 5, message = "Rating must be between 1 and 5")
     Integer rating;
 
     @Max(value = 1000, message = "Comment must be less than 1000 characters")
--- src/main/java/com/example/SWP/service/feedback/FeedbackService.java
+++ src/main/java/com/example/SWP/service/feedback/FeedbackService.java
@@ -2,14 +2,18 @@ package com.example.SWP.service.feedback;
 
 import com.example.SWP.dto.response.feedback.FeedbackResponse;
 import com.example.SWP.entity.Feedback;
+import com.example.SWP.entity.User;
+import com.example.SWP.exception.BusinessException;
 import com.example.SWP.mapper.FeedbackMapper;
 import com.example.SWP.repository.FeedbackRepository;
+import com.example.SWP.repository.UserRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.Objects;
 
 @Service
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
@@ -20,11 +24,20 @@ public class FeedbackService {
 
     FeedbackMapper feedbackMapper;
 
-    public FeedbackResponse getFeedback(Long orderId) {
+    UserRepository userRepository;
+
+    public FeedbackResponse getFeedbackFromOrder(Authentication authentication, Long orderId) {
+        User user = userRepository.findByEmail(authentication.getName()).orElseThrow(() -> new BusinessException("Không tìm thấy user", 404));
+
         Feedback feedback = feedbackRepository.findByOrder_Id(orderId);
 
         if (feedback == null) {
             return null;
+        }
+
+        if (!Objects.equals(feedback.getOrder().getSeller().getId(), user.getId()) &&
+                !Objects.equals(feedback.getOrder().getBuyer().getId(), user.getId())) {
+            throw new BusinessException("Order này không phải của bạn, bạn không thể xem feedback", 400);
         }
 
         return feedbackMapper.toFeedbackResponse(feedback);
